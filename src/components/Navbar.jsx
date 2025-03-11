ca060477831310381db022e3dad2e3af305b4ebe
Seedorf Rai <dorf123see@gmail.com>
2025-03-11 22:30:08 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../../src/assets/KevinRushLogo.png";
+
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -8,7 +8,7 @@ export default function Navbar() {
     <>
       <nav className="mb-20  flex py-6 items-center justify-between">
         <div className="flex flex-shrink-0 items-center">
-          <img className="mx-2 w-10" src={logo} alt="logo" />
+          <h2>Palash Ghosal</h2>
         </div>
         <div className="m-8 flex items-center justify-cente gap-4 text-2xl">
             <FaLinkedin></FaLinkedin>
